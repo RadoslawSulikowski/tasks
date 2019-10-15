a15c81a56da5497034f10836f1ad11134dd7cd32
RadoslawSulikowski <radoslaw.sulikowski@gmail.com>
2019-10-15 14:34:34 +0200

    Tasks.2.5(MySQL)(EmailScheduler added)

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    private String createMessage(){
+        long size = taskRepository.count();
+        return "Currently in database you got : " + size + ((size == 1) ? " task" : " tasks");
+    }
+
+    @Scheduled(cron = "0 0 10 * * *")
+    //@Scheduled(fixedDelay = 10000)
+    private void sendInformationEmail(){
+        emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT, createMessage()));
+    }
+}
